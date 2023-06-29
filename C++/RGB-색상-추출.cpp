eb21230ca7e798cf6eddd0aa6fae25c2a649451d
river-zero <lero_ro@naver.com>
2023-06-29 10:35:28 +0900

    추가: RGB-색상-추출.cpp 추가

---
+++ C++/RGB-색상-추출.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+ 
+int main() {
+    int alpha{ 255 }, red{ 255 }, green{ 0 }, blue{ 0};
+    unsigned int color;
+ 
+    color = alpha << 24 | red << 16 | green << 8 | blue;
+   
+    std::cout << std::hex;
+    std::cout << color << std::endl;
+ 
+    alpha = color >> 24;
+    red = color >> 16 & 0xFF;
+    green = color >> 8 & 0xFF;
+    blue = color & 0xFF;
+ 
+    std::cout << alpha << ", "
+              << red << ", "
+              << green << ", "
+              << blue << std::endl;
+}
--- README.md
+++ README.md
@@ -5,6 +5,7 @@ a space for what learned today.
 - [기초](C++/%EA%B8%B0%EC%B4%88.md)
 - [객체, 타입, 값](C++/%EA%B0%9D%EC%B2%B4-%ED%83%80%EC%9E%85-%EA%B0%92.md)
 - [리터럴과 연산자](C++/%EB%A6%AC%ED%84%B0%EB%9F%B4%EA%B3%BC-%EC%97%B0%EC%82%B0%EC%9E%90.md)
+  - [프로그램: RGB 색상 추출]()
 
 ## etc
 - [컴퓨터 개론](etc/%EC%BB%B4%ED%93%A8%ED%84%B0-%EA%B0%9C%EB%A1%A0.md)
